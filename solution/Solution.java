b939c82bfb77b16caf0c41380fd4b23fcacfe11f
pengpli <peng.r.li@icloud.com>
2017-02-18 18:23:12 +0800

    test acc
    https://leetcode.com/problems/reverse-nodes-in-k-group/?tab=Description

--- solution/Solution.java
+++ solution/Solution.java
@@ -6,5 +6,48 @@
 *Inputs:  1->2->3->4->5->6->7->8->NULL and k = 3 
 *Output:  3->2->1->6->5->4->7->8->NULL
 */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *
+ *     ListNode(int x) {
+         val = x; }
+ *     }
+ */
 public class Solution {
+
+	public static ListNode reverseKGroup(ListNode head, int k) {
+		if (head == null || k == 1)
+			return head;
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		ListNode pre = dummy;
+		int i = 0;
+		while (head != null) {
+			i++;
+			if (i % k == 0) {
+				pre = reverse(pre, head.next);
+				head = pre.next;
+			} else {
+				head = head.next;
+			}
+		}
+		return dummy.next;
+	}
+
+	private static ListNode reverse(ListNode pre, ListNode next) {
+		ListNode last = pre.next;// where first will be doomed "last"
+		ListNode cur = last.next;
+		while (cur != next) {
+			last.next = cur.next;
+			cur.next = pre.next;
+			pre.next = cur;
+			cur = last.next;
+		}
+		return last;
+	}
+
 }
