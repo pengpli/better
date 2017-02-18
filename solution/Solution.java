72809f5a1601a4326ee12a6149d2de5fe8bcfdf3
pengpli <peng.r.li@icloud.com>
2017-02-18 11:20:03 +0800

    Update Solution.java

--- solution/Solution.java
+++ solution/Solution.java
@@ -1,9 +1,10 @@
-#Write a program to reverse every k nodes of a linked list.
-#If the list size is not a multiple of k, then leave the remainder nodes as is.
+/*
+*Write a program to reverse every k nodes of a linked list.
+*If the list size is not a multiple of k, then leave the remainder nodes as is.
  
-#Example:
-#Inputs:  1->2->3->4->5->6->7->8->NULL and k = 3 
-#Output:  3->2->1->6->5->4->7->8->NULL
-
+*Example:
+*Inputs:  1->2->3->4->5->6->7->8->NULL and k = 3 
+*Output:  3->2->1->6->5->4->7->8->NULL
+*/
 public class Solution {
 }
