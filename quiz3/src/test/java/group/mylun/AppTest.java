a3bb47437e8241bb50150c245c33e0f2fdc08f68
pengpli <peng.r.li@icloud.com>
2017-02-19 21:37:03 +0800

    Update AppTest.java

--- quiz3/src/test/java/group/mylun/AppTest.java
+++ quiz3/src/test/java/group/mylun/AppTest.java
@@ -1,38 +1,193 @@
 package group.mylun;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import static org.hamcrest.Matchers.equalTo;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import java.util.Map;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+//import org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+public class AppTest {
+    static final String API_ENDPOINT_URL = "www.testdomain.com";
+    static final ContentType CONTENT_TYPE = null ;
+    static String sJSONBody;
+    static final String USERNAME = "foo";
+    static final String PASSWORD = "bar";
+    static final String NewSize = "5Gi";
+    static String id = "1000";
+    
+	  @Test
+	  public void getListOfLuns() {
+		  given().
+		  expect().
+		  	statusCode(200).
+		  when().
+		  	get("/lun");	  
+      }
+
+	  @Test
+	  public void getListOfLunsWithWrongToken() {
+		  given().
+          	authentication().preemptive().basic("WrongName", "WrongPassword").
+          expect().
+          	statusCode(400).
+          when().
+          	get("/lun");	  
+      }
+	  
+	  @Test	 
+	  public void addALun() {
+		  given().
+		  	parameters("id", id, "size", NewSize).
+		  expect().
+		  	body(equalTo("{\"id\": id}")).
+		  when().
+		  	put("/lun");      
+	  }
+
+	  @Test
+	  //id contain illegal character
+	  public void addALunWithIlegalID() {
+		  given().
+		  	parameters("id", "ID_HAS_ILLEGAL_CHARACTER", "size", "theSize").
+		  expect().
+          	statusCode(400).
+		  when().
+		  	put("/lun");      
+		  }
+
+	  @Test
+	  //Below zero, lager than the limit, larger than left size in the pool
+	  public void addALunWithIlegalSize() {
+		  given().
+		  parameters("id", "NEW_ID", "size", "The_Size").
+		  expect().
+          statusCode(400).
+		  when().
+		  put("/lun");      
+		  }
+
+	  @Test
+	  //id exist already
+	  public void addAExistedLun() {
+		  given().
+		  parameters("id", "ID_IN_USE", "size", "The_Size").
+		  expect().
+		  	statusCode(200).
+		  when().
+		  	put("/lun");	  
+      }
+
+	  
+	  @Test
+	  public void addMultipleLuns() {
+		  final byte[] bytes = IOUtils.toByteArray(getClass().getResourceAsStream("/path/records.json"));
+		  given().
+			content(bytes).
+		  expect().
+		  	statusCode(200).
+		  when().
+		  	put("/lun");	  
+      }
+
+	  @Test
+	  //fail if part of id illegal or the overall size overhead
+	  public void addMultipleLunsShouldFail() {
+		  final byte[] bytes = IOUtils.toByteArray(getClass().getResourceAsStream("/path/wrong_records.json"));
+		  given().
+			content(bytes).
+		  expect().
+		  	statusCode(400).
+		  when().
+		  	put("/lun");	  
+      }
+
+	  @Test
+	  public void resizeLun() {
+	        given().
+            formParam("size", NewSize).
+            expect().
+            body("size", equalTo(NewSize)).
+            when().
+            put("/lun/THE_ID");      
+      }
+
+	  @Test
+	  //fail if size illeagal or size overhead
+	  public void resizeLunShouldFail() {
+	        given().
+            	formParam("size", NewSize).
+            expect().
+            	status(400).
+            when().
+            	put("/lun/THE_ID");      
+      }
+
+	  @Test
+	  public void removeExistedLun() {
+		  given().
+		  expect().
+		  	statusCode(200).
+		  when().
+		  	delete("/lun/existed_id");	  
+      }
+
+	  @Test
+	  public void removeNonExistedLun() {
+		  given().
+		  expect().
+		  	statusCode(404).
+		  when().
+		  	delete("/lun/non_existed_id");	  
+      }
+	  
+	  @Test
+	  public void removeInUseLun() {
+		  given().
+		  expect().
+		  	statusCode(400).
+		  when().
+		  	delete("/lun/id_in_use");	  
+      }
+
+	  @Test
+	  public void getLunInfo() {
+		  given().
+		  expect().
+		  	statusCode(200).
+		  	body("size", equalTo(NewSize)).
+		  when().
+		  	get("/lun/id");	  
+      }
+	  
+	  @Test
+	  public void getLunInfoWithWrongID() {
+		  given().
+		  expect().
+		  	statusCode(400).
+		  when().
+		  	get("/lun/WRONG_ID");	  
+      }
+
+	  @Test
+	  // concurrency read should succeed
+	  // concurrency read on write, the read operation should wait for write operation
+	  // concurrency writes should fast fail.
+	  public void testConcurrency(){
+		  
+	  }
+	  
+	  @Test
+	  // test after serialization and de-serialization, the data is the same
+	  public void verifyPersistance() {
+	  
+      }	  
 }
+
+
+	  
+
