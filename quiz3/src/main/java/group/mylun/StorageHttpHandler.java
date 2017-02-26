5d080714d98a1e4f39e23b985a41b52c415f2273
pengpli <peng.r.li@icloud.com>
2017-02-26 22:00:50 +0800

    Create StorageHttpHandler.java

---
+++ quiz3/src/main/java/group/mylun/StorageHttpHandler.java
@@ -0,0 +1,11 @@
+package mylun;
+/*
+ * Handle and parse HTTP requests from client, verify user token, 
+ * do basic language check(e.g. if it's a valid JSON), 
+ * and call direct to methods in StorageCore class
+ * 
+*/
+public class StorageHttpHandler {
+	
+
+}
